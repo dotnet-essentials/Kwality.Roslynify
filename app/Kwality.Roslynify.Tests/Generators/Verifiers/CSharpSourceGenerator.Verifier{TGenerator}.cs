8832cee6f31e78c52b5ffb893a62399f28397b57
Kevin De Coninck <kevin.dconinck@gmail.com>
2023-03-09 08:46:55 +0100

    Add: "Marker" attribute
    - Add the "Marker" attribute.

---
+++ app/Kwality.Roslynify.Tests/Generators/Verifiers/CSharpSourceGenerator.Verifier{TGenerator}.cs
@@ -0,0 +1,56 @@
+﻿// =====================================================================================================================
+// = LICENSE:       Copyright (c) 2023 Kevin De Coninck
+// =
+// =                Permission is hereby granted, free of charge, to any person
+// =                obtaining a copy of this software and associated documentation
+// =                files (the "Software"), to deal in the Software without
+// =                restriction, including without limitation the rights to use,
+// =                copy, modify, merge, publish, distribute, sublicense, and/or sell
+// =                copies of the Software, and to permit persons to whom the
+// =                Software is furnished to do so, subject to the following
+// =                conditions:
+// =
+// =                The above copyright notice and this permission notice shall be
+// =                included in all copies or substantial portions of the Software.
+// =
+// =                THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// =                EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// =                OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// =                NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// =                HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// =                WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// =                FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// =                OTHER DEALINGS IN THE SOFTWARE.
+// =====================================================================================================================
+namespace Kwality.Roslynify.Tests.Generators.Verifiers;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+internal static class CSharpSourceGeneratorVerifier<TGenerator>
+    where TGenerator : IIncrementalGenerator, new()
+{
+    public sealed class Test : CSharpSourceGeneratorTest<EmptySourceGeneratorProvider, XUnitVerifier>
+    {
+        protected override IEnumerable<ISourceGenerator> GetSourceGenerators()
+        {
+            return new[] { new TGenerator().AsSourceGenerator(), };
+        }
+
+        protected override CompilationOptions CreateCompilationOptions()
+        {
+            CompilationOptions compilationOptions = base.CreateCompilationOptions();
+
+            return compilationOptions.WithSpecificDiagnosticOptions(
+                compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+        }
+
+        protected override ParseOptions CreateParseOptions()
+        {
+            return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(LanguageVersion.Default);
+        }
+    }
+}
